fd01e4cd398eb01d537885383d296cb6af2cb5bc
H4ckint0sh <alijan.adeli@icloud.com>
2020-10-17 12:18:08 +0200

    added Row component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
 
+// components
+import Row from './Row';
+import requests from './requests';
+
+
 function App() {
   return (
     <div className="app">
       <h1>frontend netflix</h1>
+      <Row 
+        title="NETFLIX ORIGINALS" 
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row 
+        title="Trending Now" 
+        fetchUrl={requests.fetchTrending} 
+      />
     </div>
   );
 }
