005e498a45d1e8560767e8a87c308ef5fca10753
H4ckint0sh <alijan.adeli@icloud.com>
2020-10-17 13:39:25 +0200

    added movie posters

--- src/Row.js
+++ src/Row.js
@@ -1,27 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import './Row.css';
+import React, { useState, useEffect } from "react";
+import "./Row.css";
 
-import axios from './axios';
+import axios from "./axios";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl }) {
-
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
-      console.log(request);
-
+      setMovies(request.data.results);
       return request;
     }
     fetchData();
-  }, [fetchUrl])
+  }, [fetchUrl]);
 
+  console.log(movies);
   return (
     <div className="row">
       <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Row;
